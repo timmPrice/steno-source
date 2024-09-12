a7d41324a612ac5aea7ff78c4b1484a3a11da8d3
timmPrice <timmPrice6@gmail.com>
2024-09-12 14:51:52 -0400

    adding extra layer onto stenosource keymap

--- stenosource/keymaps/steno/keymap.c
+++ stenosource/keymaps/steno/keymap.c
@@ -5,14 +5,20 @@
 
 enum stenosplit_layers {
     _PLOVER,
+    _Symbols,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-    [_PLOVER] = LAYOUT_stenosouce(
-        KC_NO, STN_S1, STN_TL, STN_PL, STN_HL, STN_ST1,    STN_FR, STN_PR, STN_LR, STN_TR, STN_DR, KC_NO,
-        KC_NO, STN_S2, STN_KL, STN_WL, STN_RL, STN_ST2,    STN_RR, STN_BR, STN_GR, STN_SR, STN_ZR, KC_NO,
-                                  STN_N1, STN_A, STN_O,    STN_E, STN_U, STN_N2
+    [_PLOVER] = LAYOUT_stenosource(
+       TG(_Symbols), STN_S1, STN_TL, STN_PL, STN_HL, STN_ST1,    STN_ST3, STN_FR, STN_PR, STN_LR, STN_TR, STN_DR,
+              KC_NO, STN_S2, STN_KL, STN_WL, STN_RL, STN_ST2,    STN_ST4, STN_RR, STN_BR, STN_GR, STN_SR, STN_ZR,
+                                        STN_N1, STN_A, STN_O,    STN_E, STN_U, STN_N2
+    ),
+    [_Symbols] = LAYOUT_stenosource(
+       TG(_PLOVER), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+             KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+                                  KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO, KC_NO
     )
 };
 
